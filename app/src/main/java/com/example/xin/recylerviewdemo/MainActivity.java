f77118882e8809f4bb9ecc391f25a8b11d44571a
zjx <1070386906@qq.com>
2017-09-26 20:02:16 +0800

    添加分割线

--- app/src/main/java/com/example/xin/recylerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/xin/recylerviewdemo/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.xin.recylerviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private List<String> mDatas;
+    private HomeAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        mRecyclerView = (RecyclerView) findViewById(R.id.id_RecyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL_LIST));
+    }
+    protected void initData(){
+        mDatas = new ArrayList<String>();
+        for (int i = '0';i < 'z';i++){
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>{
+
+        @Override
+        public HomeAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    MainActivity.this).inflate(R.layout.item_home,parent,false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(HomeAdapter.MyViewHolder holder, int position) {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder{
+
+            TextView tv;
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                tv = (TextView) itemView.findViewById(R.id.id_num);
+            }
+        }
     }
 }
